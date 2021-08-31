425f1877445aa6c825b8adb8c626d6ab7f26c70d
SOOS-PChen <87992343+SOOS-Pchen@users.noreply.github.com>
2021-08-31 16:51:09 -0400

    Add files via upload

---
+++ src/cli/myshell.sh
@@ -0,0 +1,46 @@
+# run soos.py with the -h flag for help
+# REQUIRED ARGS:
+SOOS_PROJECT_NAME="PJ_testing_new_wrapper"
+
+# BUILD/BRANCH SPECIFIC ARGS:
+SOOS_COMMIT_HASH=""                # ENTER COMMIT HASH HERE IF KNOWN
+SOOS_BRANCH_NAME=""                # ENTER BRANCH NAME HERE IF KNOWN
+SOOS_BRANCH_URI=""                 # ENTER BRANCH URI HERE IF KNOWN
+SOOS_BUILD_VERSION=""              # ENTER BUILD VERSION HERE IF KNOWN
+SOOS_BUILD_URI=""                  # ENTER BUILD URI HERE IF KNOWN
+SOOS_OPERATING_ENVIRONMENT=""      # ENTER OPERATING ENVIRONMENT HERE IF KNOWN (default will be provided)
+SOOS_INTEGRATION_NAME="Script"
+
+# OPTIONAL ARGS:
+WORKSPACE="C:/Users/pchen/Videos/REPO2"
+SOOS_MODE="run_and_wait"
+SOOS_ON_FAILURE="fail_the_build"
+SOOS_DIRS_TO_EXCLUDE="soos"
+SOOS_FILES_TO_EXCLUDE=""
+SOOS_ANALYSIS_RESULT_MAX_WAIT=300
+SOOS_ANALYSIS_RESULT_POLLING_INTERVAL=10
+SOOS_CHECKOUT_DIR="./"
+
+SOOS_API_BASE_URL="https://dev-api.soos.io/api/"
+
+
+
+# **************************** Modify Above Only ***************#
+mkdir -p "${WORKSPACE}/soos/workspace"
+
+cd "${WORKSPACE}/soos"
+
+curl -s https://api.github.com/repos/soos-io/soos-ci-analysis-python/releases/latest | grep "browser_download_url" | cut -d '"' -f 4 | xargs -n 1 curl -LO
+sha256sum -c soos.sha256
+sha256sum -c requirements.sha256
+
+cd ${WORKSPACE}
+
+python -m venv ./
+
+cd Scripts
+source activate
+cd ${WORKSPACE}
+
+pip3 install -r soos/requirements.txt
+python soos/soos.py -m="${SOOS_MODE}" -of="${SOOS_ON_FAILURE}" -dte="${SOOS_DIRS_TO_EXCLUDE}" -fte="${SOOS_FILES_TO_EXCLUDE}" -wd="${SOOS_CHECKOUT_DIR}" -armw=${SOOS_ANALYSIS_RESULT_MAX_WAIT} -arpi=${SOOS_ANALYSIS_RESULT_POLLING_INTERVAL} -buri="${SOOS_API_BASE_URL}" -scp="${SOOS_CHECKOUT_DIR}" -pn="${SOOS_PROJECT_NAME}" -ch="${SOOS_COMMIT_HASH}" -bn="${SOOS_BRANCH_NAME}" -bruri="${SOOS_BRANCH_URI}" -bldver="${SOOS_BUILD_VERSION}" -blduri="${SOOS_BUILD_URI}" -oe="${SOOS_OPERATING_ENVIRONMENT}" -intn="${SOOS_INTEGRATION_NAME}"
