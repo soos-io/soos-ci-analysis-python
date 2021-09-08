8c02d1a028a64c6a20588e47f114cfc701e1c734
PEIJIN CHEN <PCHEN@SOOS.IO>
2021-09-08 18:20:10 -0400

    slight changes to paths in the script

--- src/cli/soos_wrapper.sh
+++ src/cli/soos_wrapper.sh
@@ -29,13 +29,13 @@ SOOS_API_BASE_URL="https://api.soos.io/api/"
 # **************************** Modify Above Only ***************#
 mkdir -p "${WORKSPACE}/soos/workspace"
 
-cd "${WORKSPACE}/soos"
+cd "${WORKSPACE}/soos/workspace"
 
 curl -s https://api.github.com/repos/soos-io/soos-ci-analysis-python/releases/latest | grep "browser_download_url" | cut -d '"' -f 4 | xargs -n 1 curl -LO
 sha256sum -c soos.sha256
 sha256sum -c requirements.sha256
 
-cd ${WORKSPACE}
+cd "${WORKSPACE}/soos"
 
 python -m venv ./ #might need python3
 
@@ -43,9 +43,9 @@ cd Scripts
 source activate
 cd ${WORKSPACE}
 
-pip3 install -r soos/requirements.txt #might need pip3 
+pip3 install -r soos/workspace/requirements.txt #might need pip3 
 
 #might need python3 instead of python
 
 
-python soos/soos.py -m="${SOOS_MODE}" -of="${SOOS_ON_FAILURE}" -dte="${SOOS_DIRS_TO_EXCLUDE}" -fte="${SOOS_FILES_TO_EXCLUDE}" -wd="${SOOS_CHECKOUT_DIR}" -armw=${SOOS_ANALYSIS_RESULT_MAX_WAIT} -arpi=${SOOS_ANALYSIS_RESULT_POLLING_INTERVAL} -buri="${SOOS_API_BASE_URL}" -scp="${SOOS_CHECKOUT_DIR}" -pn="${SOOS_PROJECT_NAME}" -ch="${SOOS_COMMIT_HASH}" -bn="${SOOS_BRANCH_NAME}" -bruri="${SOOS_BRANCH_URI}" -bldver="${SOOS_BUILD_VERSION}" -blduri="${SOOS_BUILD_URI}" -oe="${SOOS_OPERATING_ENVIRONMENT}" -intn="${SOOS_INTEGRATION_NAME}"
+python soos/workspace/soos.py -m="${SOOS_MODE}" -of="${SOOS_ON_FAILURE}" -dte="${SOOS_DIRS_TO_EXCLUDE}" -fte="${SOOS_FILES_TO_EXCLUDE}" -wd="${SOOS_CHECKOUT_DIR}" -armw=${SOOS_ANALYSIS_RESULT_MAX_WAIT} -arpi=${SOOS_ANALYSIS_RESULT_POLLING_INTERVAL} -buri="${SOOS_API_BASE_URL}" -scp="${SOOS_CHECKOUT_DIR}" -pn="${SOOS_PROJECT_NAME}" -ch="${SOOS_COMMIT_HASH}" -bn="${SOOS_BRANCH_NAME}" -bruri="${SOOS_BRANCH_URI}" -bldver="${SOOS_BUILD_VERSION}" -blduri="${SOOS_BUILD_URI}" -oe="${SOOS_OPERATING_ENVIRONMENT}" -intn="${SOOS_INTEGRATION_NAME}"
