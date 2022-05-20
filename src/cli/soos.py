3ff56dab76da9044bb8177dae5aa176e93f240ed
mmalony <mmalony@soos.io>
2022-05-20 07:52:12 -0700

    PA-4616 Simplified operating environment check

--- src/cli/soos.py
+++ src/cli/soos.py
@@ -427,11 +427,8 @@ class SOOSContext:
 
         # Operating environment, if missing, will default to platform
 
-        if script_args.operating_environment is not None:
-            if len(script_args.operating_environment) > 0:
-                self.operating_environment = str(script_args.operating_environment)
-            else:
-                self.operating_environment = '{system} {release} {architecture}'.format(system=platform.system(), release=platform.release(), architecture=platform.architecture()[0])
+        if script_args.operating_environment is not None and len(script_args.operating_environment) > 0:
+            self.operating_environment = str(script_args.operating_environment)
         else:
             self.operating_environment = '{system} {release} {architecture}'.format(system=platform.system(), release=platform.release(), architecture=platform.architecture()[0])
         SOOS.console_log("SOOS_OPERATING_ENVIRONMENT Parameter Loaded: " + self.operating_environment)
