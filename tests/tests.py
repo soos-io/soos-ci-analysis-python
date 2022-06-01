1bd042c96a0e010b47f91436f514da6f3bf0bba3
Greg Valentine <gregjoeval@gmail.com>
2022-06-01 16:48:05 -0400

    undo changes to tests.py

--- tests/tests.py
+++ tests/tests.py
@@ -1,4 +1,4 @@
-import pathlib
+import os
 import subprocess
 import unittest
 
@@ -17,8 +17,8 @@ def violations(x):
 class GemfileTestCases(unittest.TestCase):
     def test_gemfile(self):
         print("testing gemfile")
-        process = subprocess.run([pathlib.Path("./manifests/gemfile/script.sh").resolve()], shell=True, capture_output=True, text=True)
-
+        process = subprocess.run(["sh", f"{os.getcwd()}/manifests/gemfile/script.sh"], capture_output=True,
+                                 text=True)
         print(process.stdout)
         self.assertEqual(process.returncode, 1, "Invalid return code.")
         self.assertEqual(process.stdout.count(test_complete_fail), 1, "Invalid completion message.")
@@ -29,7 +29,8 @@ class GemfileTestCases(unittest.TestCase):
 class DartTestCases(unittest.TestCase):
     def test_dart(self):
         print("testing dart")
-        process = subprocess.run([pathlib.Path("./manifests/dart/script.sh").resolve()], shell=True, capture_output=True, text=True)
+        process = subprocess.run(["sh", f"{os.getcwd()}/manifests/dart/script.sh"], capture_output=True,
+                                 text=True)
 
         print(process.stdout)
         self.assertEqual(process.returncode, 0, "Invalid return code.")
@@ -41,7 +42,8 @@ class DartTestCases(unittest.TestCase):
 class GradleTestCases(unittest.TestCase):
     def test_gradle(self):
         print("testing gradle")
-        process = subprocess.run([pathlib.Path("./manifests/gradle/script.sh").resolve()], shell=True, capture_output=True, text=True)
+        process = subprocess.run(["sh", f"{os.getcwd()}/manifests/gradle/script.sh"], capture_output=True,
+                                 text=True)
 
         print(process.stdout)
         self.assertEqual(process.returncode, 1, "Invalid return code.")
@@ -53,7 +55,8 @@ class GradleTestCases(unittest.TestCase):
 class RustTestCases(unittest.TestCase):
     def test_rust(self):
         print("testing rust")
-        process = subprocess.run([pathlib.Path("./manifests/rust/script.sh").resolve()], shell=True, capture_output=True, text=True)
+        process = subprocess.run(["sh", f"{os.getcwd()}/manifests/rust/script.sh"], capture_output=True,
+                                 text=True)
 
         print(process.stdout)
         self.assertEqual(process.returncode, 1, "Invalid return code.")
@@ -65,7 +68,8 @@ class RustTestCases(unittest.TestCase):
 class NugetTestCases(unittest.TestCase):
     def test_nuget(self):
         print("testing nuget")
-        process = subprocess.run([pathlib.Path("./manifests/nuget/script.sh").resolve()], shell=True, capture_output=True, text=True)
+        process = subprocess.run(["sh", f"{os.getcwd()}/manifests/nuget/script.sh"], capture_output=True,
+                                 text=True)
 
         print(process.stdout)
         self.assertEqual(process.returncode, 1, "Invalid return code.")
@@ -77,15 +81,17 @@ class NugetTestCases(unittest.TestCase):
 # class MavenTestCases(unittest.TestCase):
 #     def test_maven(self):
 #         print("testing maven")
-#         process = subprocess.run([pathlib.Path("./manifests/maven/script.sh").resolve()], shell=True, capture_output=True, text=True)
-
+#         process = subprocess.run(["sh", f"{os.getcwd()}/manifests/maven/script.sh"], capture_output=True,
+#                                  text=True)
+#
 #         self.assertEqual(process.returncode, 0)
 
 
 class ComposerTestCases(unittest.TestCase):
     def test_composer(self):
         print("testing composer")
-        process = subprocess.run([pathlib.Path("./manifests/composer/script.sh").resolve()], shell=True, capture_output=True, text=True)
+        process = subprocess.run(["sh", f"{os.getcwd()}/manifests/composer/script.sh"], capture_output=True,
+                                 text=True)
 
         print(process.stdout)
         self.assertEqual(process.returncode, 0, "Invalid return code.")
@@ -95,16 +101,15 @@ class ComposerTestCases(unittest.TestCase):
 class NPMTestCases(unittest.TestCase):
     def test_with_issues(self):
         print("testing with issues")
-        process = subprocess.run([pathlib.Path("./manifests/with_issues/script.sh").resolve()], shell=True, capture_output=True, text=True)
-
+        process = subprocess.run(["sh", f"{os.getcwd()}/manifests/with_issues/script.sh"], capture_output=True,
+                                 text=True)
         print(process.stdout)
         self.assertEqual(process.returncode, 1, "Invalid return code.")
         self.assertEqual(process.stdout.count(test_complete_fail), 1, "Invalid completion message.")
 
     def test_no_issues(self):
         print("testing without issues")
-        process = subprocess.run([pathlib.Path("./manifests/no_issues/script.sh").resolve()], shell=True, capture_output=True, text=True)
-
+        process = subprocess.run(["sh", f"{os.getcwd()}/manifests/no_issues/script.sh"], capture_output=True, text=True)
         print(process.stdout)
         self.assertEqual(process.returncode, 0, "Invalid return code.")
         self.assertEqual(process.stdout.count(test_complete_success), 1, "Invalid completion message.")
@@ -113,8 +118,8 @@ class NPMTestCases(unittest.TestCase):
 class ExcludeTestCases(unittest.TestCase):
     def test_exclude_files(self):
         print("testing exclude files")
-        process = subprocess.run([pathlib.Path("./manifests/exclude_files/script.sh").resolve()], shell=True, capture_output=True, text=True)
-
+        process = subprocess.run(["sh", f"{os.getcwd()}/manifests/exclude_files/script.sh"], capture_output=True,
+                                 text=True)
         print(process.stdout)
         self.assertEqual(process.returncode, 1, "Invalid return code.")
         files_to_exclude = "pubspec.yaml, *composer.json, cargo* "
@@ -130,8 +135,8 @@ class ExcludeTestCases(unittest.TestCase):
 
     def test_exclude_dirs(self):
         print("testing exclude dirs")
-        process = subprocess.run([pathlib.Path("./manifests/exclude_dirs/script.sh").resolve()], shell=True, capture_output=True, text=True)
-
+        process = subprocess.run(["sh", f"{os.getcwd()}/manifests/exclude_dirs/script.sh"], capture_output=True,
+                                 text=True)
         print(process.stdout)
         self.assertEqual(process.returncode, 1, "Invalid return code.")
         self.assertEqual(process.stdout.count('Skipping file due to dirs_to_exclude:'), 3)
@@ -145,8 +150,9 @@ class ExcludeTestCases(unittest.TestCase):
 # class SarifTestCases(unittest.TestCase):
 #     def test_sarif(self):
 #         print("testing sarif")
-#         process = subprocess.run([pathlib.Path("./manifests/sarif/script.sh").resolve()], shell=True, capture_output=True, text=True)
-
+#         process = subprocess.run(["sh", f"{os.getcwd()}/manifests/sarif/script.sh"], capture_output=True,
+#                                  text=True)
+#
 #         print(process.stdout)
 #         self.assertEqual(process.returncode, 0)
 #         self.assertEqual(process.stdout.count(test_complete_success), 1)
